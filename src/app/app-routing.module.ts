222bff10a823825debdec18f7319a306587a17f3
Umang Pandya <umang6891@gmail.com>
2019-05-22 15:40:13 +0530

    - Project Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/* Starting Componenets */
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { IndexComponent } from './app-website/index/index.component';
+import { BaseComponent } from './app-website/base/base.component';
+/* End Componenets */
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [
+      {
+        path: '',
+        component: IndexComponent,
+      },
+      // {
+      //   path: 'about-me',
+      //   component: AppAboutComponent,
+      // },
+      // {
+      //   path: 'skill',
+      //   component: AppSkillComponent,
+      // },
+      // {
+      //   path: 'experience',
+      //   component: AppExperienceComponent,
+      // },
+      // {
+      //   path: 'education',
+      //   component: AppEducationComponent,
+      // },
+      // {
+      //   path: 'my-work',
+      //   component: AppMyWorkComponent,
+      // },
+      // {
+      //   path: 'contact-me',
+      //   component: AppContactMeComponent,
+      // },
+      // {
+      //   path: 'profile/institute/:university_id',
+      //   component: ProfileInstituteComponent,
+      // },
+      // {
+      //   path: '**',
+      //   component: Error404Component,
+      // }
+    ]
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+/* Starting Componenets */
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { IndexComponent } from './app-website/index/index.component';
+import { BaseComponent } from './app-website/base/base.component';
+/* End Componenets */
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    IndexComponent,
+    BaseComponent,
   ],
   imports: [
     BrowserModule,
